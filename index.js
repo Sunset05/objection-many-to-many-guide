148829e036f21d0ac01aee0d122674f2c5249a09
Keith Funk <Keith.C.Funk@gmail.com>
2021-07-19 21:29:11 -0600

    objection setup, models, and relations setup to return relationships as json

--- index.js
+++ index.js
@@ -2,20 +2,61 @@ const express = require("express");
 const cors = require("cors");
 const knex = require("knex");
 const databaseConfig = require("./knexfile").development
+const port = process.env.PORT || 4000
+
+const { Model } = require("objection")
 
 const app = express();
 const database = knex(databaseConfig);
+Model.knex(database)
 
 app.use(cors());
 
-app.get("/students", (request, response) => {
-    const students = [{ id: 1 }]
+class Course extends Model {
+    static tableName = "course"
+}
 
-    response.json({students})
-})
+class Student extends Model {
+    static tableName = "student"
+    static relationMappings = {
+        courses: {
+            relation: Model.ManyToManyRelation,
+            modelClass: Course,
+            join: {
+                from: "student.id",
+                through: {
+                    from: "enrollment.student_id",
+                    to: "enrollment.course_id"
+                },
+                to: "course.id",
+
+            } 
+        }
+    };
+}
+
+
+app.get("/students", (request, response) => {
+    Student.query().withGraphFetched("courses")
+        .then(students => {
+            response.json({students})
+        }).catch(error => {
+            console.error(error.message)
+            response.sendStatus(500)
+        })
+});
+
+app.get("/courses", (request, response) => {
+    Course.query()
+        .then(courses => {
+            response.json({courses})
+        })
+});
 
 app.get("/", (request, response) => {
     response.json({ message: "You're in flavor country!" })
 })
 
-app.listen(4000);
+app.listen(port, () => {
+    console.log(`I AM LISTENING ON PORT ${port}`)
+});
